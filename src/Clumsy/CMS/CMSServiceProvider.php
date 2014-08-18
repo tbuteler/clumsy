fc60d73c8e27e681c4d56bd678f16f839ac163e0
Tomas Buteler <tbuteler@gmail.com>
2014-08-18 09:15:26 +0100

    Shifted some features over to clumsy/utils

--- src/Clumsy/CMS/CMSServiceProvider.php
+++ src/Clumsy/CMS/CMSServiceProvider.php
@@ -20,7 +20,9 @@ class CMSServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		$this->app->register('Cartalyst\Sentry\SentryServiceProvider');
+		$this->app->register('Clumsy\Locale\LocaleServiceProvider');
 		$this->app->register('Clumsy\Assets\AssetsServiceProvider');
+		$this->app->register('Clumsy\Utils\UtilsServiceProvider');
 		$this->app->register('Clumsy\Eminem\EminemServiceProvider');
 	}
 
@@ -37,14 +39,9 @@ class CMSServiceProvider extends ServiceProvider {
         $admin_assets = include($this->guessPackagePath() . '/assets/assets.php');
 		Asset::batchRegister($admin_assets);
 
-		require $this->guessPackagePath().'/helpers.php';
-
 		require $this->guessPackagePath().'/filters.php';
 		require $this->guessPackagePath().'/routes.php';
 		require $this->guessPackagePath().'/errors.php';
-
-		require $this->guessPackagePath().'/macros/form.php';
-		require $this->guessPackagePath().'/macros/string.php';
 	}
 
 	/**
