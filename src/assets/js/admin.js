11f689b0adc1ea31cdc2a00a2cb2bbc099556b40
Tomas Buteler <tomas.buteler@wiz.pt>
2014-09-10 16:03:37 +0100

    TinyMCE now resizes to fit content; TinyMCE init options can now be customized by passing an object via handover

--- src/assets/js/admin.js
+++ src/assets/js/admin.js
@@ -7,13 +7,17 @@ $(function(){
     });
 
     if ($('.rich-text').length) {
-        tinymce.init({
+        tinymce.init($.extend(
+        {
             selector: ".rich-text",
             content_css: handover.admin.base_url+'/../packages/clumsy/cms/css/tinymce.css',
             menubar : false,
             toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright | bullist numlist",
-            statusbar : false
-        });
+            statusbar : false,
+            plugins: "autoresize"
+        },
+        typeof handover.admin.tinymce === 'undefined' ? {} : handover.admin.tinymce
+        ));
     }
 
     $('#is_video_radio input').change(function(){
