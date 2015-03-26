2b9bb46d7f04d96add6a4d186e161f3ce1726ac4
Tomas Buteler <tomas.buteler@wiz.pt>
2015-03-26 17:54:27 +0000

    Fixed variable typo

--- src/views/templates/edit.blade.php
+++ src/views/templates/edit.blade.php
@@ -18,14 +18,14 @@
 
     <div class="bottom-buttons">
         {{ Form::button(isset($item) ? trans('clumsy::buttons.save') : trans('clumsy::buttons.create'), array('type' => 'submit', 'class' => 'btn btn-lg btn-success')) }}
-        @if (isset($item) && !isset($supress_delete))
+        @if (isset($item) && !isset($suppress_delete))
             {{ Form::button('', array('type' => 'button', 'title' => trans('clumsy::buttons.delete'), 'class' => 'delete btn btn-lg btn-default glyphicon glyphicon-trash')) }}
         @endif
     </div>
 
     {{ Form::close() }}
 
-    @if (isset($item) && !isset($supress_delete))
+    @if (isset($item) && !isset($suppress_delete))
     
         {{ Form::delete($resource, $item->id) }}
 
