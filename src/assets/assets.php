2650663724b24a066a098b9244fda858a3c5172c
Tomas Buteler <tomas.buteler@wiz.pt>
2015-04-22 17:00:11 +0100

    Minor styling fix

--- src/assets/assets.php
+++ src/assets/assets.php
@@ -6,14 +6,14 @@ return array(
         'set'   => 'styles',
         'path'  => 'packages/clumsy/cms/css/admin.css',
         'req'   => 'bootstrap',
-        'v'     => '0.9.3',
+        'v'     => '0.16.0',
     ),
 
     'admin.js' => array(
         'set'   => 'footer',
         'path'  => 'packages/clumsy/cms/js/admin.min.js',
         'req'   => array('bootstrap.js','chosen'),
-        'v'     => '0.9.3',
+        'v'     => '0.16.0',
     ),
 
     'google-maps' => array(
