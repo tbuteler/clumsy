35913b91466bf1cf2f57960575b91f3ff852d1bf
Tomas Buteler <tomas.buteler@wiz.pt>
2015-11-25 20:36:50 +0000

    Fixed Hashid trait -- it should now generate actual unique hashes

--- src/Models/Traits/Hashid.php
+++ src/Models/Traits/Hashid.php
@@ -2,15 +2,26 @@
 
 namespace Clumsy\CMS\Models\Traits;
 
+use Carbon\Carbon;
 use Hashids\Hashids;
 
 trait Hashid
 {
-    public static function bootSluggable()
+    public static function bootHashid()
     {
         self::creating(function ($model) {
-            $hasher = new Hashids(config('app.key'), 6);
-            $model->hash_id = $hasher->encode($model->getKey());
+            $hasher = new Hashids(config('app.key'));
+            $datetime = Carbon::now();
+            $model->hash_id = $hasher->encode(
+                substr((string)$datetime->year, 1, 3),
+                $datetime->month,
+                $datetime->day,
+                $datetime->hour,
+                $datetime->minute,
+                $datetime->second,
+                round(head(explode(' ', microtime()))*100),
+                rand(0,9)
+            );
         });
     }
 }
