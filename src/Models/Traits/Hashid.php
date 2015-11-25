02a3c1d9ee8f7bf3a06746a8a7813d946d85b529
Tomas Buteler <tomas.buteler@wiz.pt>
2015-11-25 22:56:40 +0000

    Improvements in Hashid trait

--- src/Models/Traits/Hashid.php
+++ src/Models/Traits/Hashid.php
@@ -10,18 +10,49 @@ trait Hashid
     public static function bootHashid()
     {
         self::creating(function ($model) {
-            $hasher = new Hashids(config('app.key'));
-            $datetime = Carbon::now();
-            $model->hash_id = $hasher->encode(
-                substr((string)$datetime->year, 1, 3),
-                $datetime->month,
-                $datetime->day,
-                $datetime->hour,
-                $datetime->minute,
-                $datetime->second,
-                round(head(explode(' ', microtime()))*100),
-                rand(0,9)
-            );
+            if ($model->hashIdBeforeCreating()) {
+                $model->hash_id = $model->hashByTimestamp();
+            } else {
+                self::created(function ($model) {
+                    $model->hash_id = $model->hashByPrimaryKey();
+                    $model->save();
+                });
+            }
         });
+    }
+
+    public function minimumHashIdSize()
+    {
+        return property_exists($this, 'minimumHashIdSize') ? $this->minimumHashIdSize : 5;
+    }
+
+    public function hasher()
+    {
+        return new Hashids(config('app.key'), $this->minimumHashIdSize());
+    }
+
+    public function hashIdBeforeCreating()
+    {
+        return property_exists($this, 'hashIdBeforeCreating') && $this->hashIdBeforeCreating;
+    }
+
+    public function hashByTimestamp()
+    {
+        $datetime = Carbon::now();
+        return $this->hasher()->encode(
+            substr((string)$datetime->year, 1, 3),
+            $datetime->month,
+            $datetime->day,
+            $datetime->hour,
+            $datetime->minute,
+            $datetime->second,
+            round(head(explode(' ', microtime()))*1000),
+            rand(0,9)
+        );
+    }
+
+    public function hashByPrimaryKey()
+    {
+        return $this->hasher()->encode($this->getKey());
     }
 }
