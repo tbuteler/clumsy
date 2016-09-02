ff0893ac725276d775161b39fd8f55d763452244
Tomas Buteler <tomas.buteler@wiz.pt>
2016-09-02 17:57:09 +0100

    Use new app name from config if no admin title is set

--- src/views/templates/navbar-home-link.blade.php
+++ src/views/templates/navbar-home-link.blade.php
@@ -1,5 +1,5 @@
 <a class="navbar-brand" href="{{ url("$adminPrefix") }}">
 @section('admin-title')
-    {!! $admin_title or '<span class="visible-xs">Home</span><span class="glyphicon glyphicon-home hidden-xs"></span>' !!}
+    {{ $admin_title or config('app.name') }}
 @show
 </a>
