0c850ed9c0863b379ad3ce50b15b425e917cca1e
Tomas Buteler <tomas.buteler@wiz.pt>
2016-09-02 18:24:54 +0100

    Show empty table on empty gallery too

--- src/views/templates/gallery.blade.php
+++ src/views/templates/gallery.blade.php
@@ -2,23 +2,34 @@
 
     {!! csrf_field() !!}
 
-	@foreach ($items as $i => $item)
-		<div class="col-md-{{ $columnSize }}">
-			<div class="thumbnail gallery-item {{ $panel->rowClass($item) }}">
-				<a href="{{ route("$routePrefix.edit", $item->id) }}">
-					{!! $panel->galleryThumbnail($item) !!}
-				</a>
-				<div class="caption">
-					@foreach ($columns as $column => $name)
-	                    <p>{!! $panel->columnValue($item, $column) !!}</p>
-	                @endforeach
-				</div>
-			</div>
-		</div>
+        @if (count($items))
 
-		@if (($i+1) % $columnsPerRow == 0)
-			<div class="clearfix"></div>
-		@endif
+        @foreach ($items as $i => $item)
+            <div class="col-md-{{ $columnSize }}">
+                <div class="thumbnail gallery-item {{ $panel->rowClass($item) }}">
+                    <a href="{{ route("$routePrefix.edit", $item->id) }}">
+                        {!! $panel->galleryThumbnail($item) !!}
+                    </a>
+                    <div class="caption">
+                        @foreach ($columns as $column => $name)
+                            <p>{!! $panel->columnValue($item, $column) !!}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
 
-    @endforeach
+            @if (($i+1) % $columnsPerRow == 0)
+                <div class="clearfix"></div>
+            @endif
+
+        @endforeach
+
+    @else
+
+        <div class="panel panel-default">
+            @include($view->resolve('table-empty'))
+        </div>
+
+    @endif
+
 </div>
