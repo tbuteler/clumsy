0e2e0204fff9b359b43fbb5fe66950c1fb0e5fa1
Filipe Santos <flipjms@gmail.com>
2016-10-25 18:45:50 +0100

    Fixed issue which sets the default auth guard to clumsy when caching the config file.

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -251,4 +251,14 @@ class AuthController extends Controller
             'success' => trans('clumsy::alerts.auth.logged-out')
         ]);
     }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Overseer::guard();
+    }
 }
